a53b7204e4b7f5e8240da6c9a8f260241aedd2e9
Gene <g-nakagaki@ceres-inc.jp>
2022-05-26 19:52:19 +0900

    feat: setup Laravel stuff

--- app/Layers/Web/Controller/Controller.php
+++ app/Layers/Web/Controller/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Layers\Web\Controller;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
